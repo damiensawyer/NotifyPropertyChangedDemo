bb9cf152f8a2a6d141d1682edfd99012bfb5da81
Damien Sawyer <damiensawyer@gmail.com>
2021-05-13 03:10:25 +0100

    whitespace

---
+++ NotifyPropertyChangedLiveDemo/PlaneModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace NotifyPropertyChangedLiveDemo
+{
+    public partial class PlaneModel : INotifyPropertyChanged
+    {
+        private double SpeedKmPerHourBackingField;
+        private int NumberOfDoorsBackingField;
+        private string ModelBackingField = "";
+        private string DamienName3BackingField = "";
+        private string DamienName4BackingField = "";
+    
+    
+
+        public void SpeedUp() => SpeedKmPerHour *= 1.1;
+    }
+}
